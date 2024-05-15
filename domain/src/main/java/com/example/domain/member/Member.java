b208d112b0da232b3c890e5709617f11d7a4b1e3
prebird <78974381+prebird@users.noreply.github.com>
2024-05-15 22:01:26 +0900

    feat: 회원 엔티티 추가

---
+++ domain/src/main/java/com/example/domain/member/Member.java
@@ -0,0 +1,12 @@
+package com.example.domain.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+  @Id @GeneratedValue
+  private Long id;
+  private String name;
+}
